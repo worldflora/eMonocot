21b2529a7489fd2192689f69f19b0f8646d180bd
Nye Hughes <nye@dalrymple.eu>
2021-05-04 13:55:45 +0100

    Fix: Incorrect placing of fixed sub-nav menu on taxon pages; Fix: broken smooth scroll into view

--- emonocot-static/src/main/js/application.js
+++ emonocot-static/src/main/js/application.js
@@ -5,7 +5,7 @@
     // fix sub nav on scroll
     var $win = $(window)
       , $nav = $('.subnav')
-      , navTop = $('.subnav').length && $('.subnav').offset().top - 48
+      , navTop = $('.subnav').length && $('.subnav').offset().top
       , isFixed = 0;
 
     processScroll();
@@ -17,6 +17,8 @@
       var i, scrollTop = $win.scrollTop();
       if (scrollTop >= navTop && !isFixed) {
         isFixed = 1;
+        console.log (scrollTop);
+        console.log($('.subnav').offset().top)
         $nav.addClass('subnav-fixed');
       } else if (scrollTop <= navTop && isFixed) {
         isFixed = 0;
